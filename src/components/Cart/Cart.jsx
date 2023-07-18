5e7a8fbbceb3fe90b61cf16d52d2286aeb929648
Gourav <Gouravkumar.1273@gmail.com>
2023-07-19 00:20:28 +0530

    Implementing the cart Itemns page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,32 @@
-import Modal from '../UI/Modal';
+ 
+import React ,{useContext} from 'react';
+import Modal from '../UI/Modal'; 
 import classes from './Cart.module.css';
+import CartCntxt from '../../store/Cart-context'
 
 const Cart = (props) => {
+
+  const cartcntx=useContext(CartCntxt);
+  let totalAmount=0;
+  cartcntx.items.forEach(item=>{
+    totalAmount= totalAmount+ (Number(item.price)*Number(item.quantity));
+  })
+
+  const combinedItems={};
+  cartcntx.items.forEach((item)=>{
+    const itemName = item.name;
+    const itemQuantity = Number(item.quantity);
+    const itemPrice = Number(item.price);
+    if (combinedItems[itemName]) 
+      combinedItems[itemName].quantity += itemQuantity;
+    else combinedItems[itemName] = { ...item, quantity: itemQuantity, price: itemPrice };
+  })
+
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      { Object.keys(combinedItems).map((item) => (
+        <li style={{wordSpacing:'10px'}}>Name: {item} Price: {combinedItems[item].price} Quantity: {combinedItems[item].quantity} </li>
       ))}
     </ul>
   );
@@ -15,7 +36,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']  } onClick={props.onClose}>
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import classes from "./MealItemForm.module.css"
 import Input from '../../UI/Input'
+import CartContext from '../../../store/Cart-context'
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+  const cartcntx=useContext(CartContext)
+
+
+  const addItemToCart=(e)=>{
+    //update cartcntx.items
+    e.preventDefault();
+    const quantity=document.getElementById('amount_'+props.id).value
+    cartcntx.addItem({...props.item , quantity:quantity})
+ 
+
+
+
+  };
+
   return (
     <form className={classes.form}>
+      {console.log('inside render' , cartcntx.items)}
          <Input
             label="Amount"
             input={{
-                id:'amount',
+                id:'amount_'+props.id,
                 type:'number',
                 min:'1',
                 max:'5',
@@ -16,7 +32,7 @@ const MealItemForm = () => {
                 defaultValue:'1'
             }}
          />
-        <button>+ Add</button>
+        <button onClick={addItemToCart}>+ Add</button>
     </form>
   )
 }
